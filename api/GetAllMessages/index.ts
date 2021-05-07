9fa5ea88ee9ad5ccee06d7ab21283b0f752c1aa2
Aidan Higginbotham <aidanhigginbo265714@gmail.com>
2021-05-07 13:43:47 -0500

    feat: test

--- api/GetAllMessages/index.ts
+++ api/GetAllMessages/index.ts
@@ -1,29 +1,19 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import { PrismaClient } from "@prisma/client";
-
 type Message = {
     id: number,
     name: string,
     message: string
 }
 
-const prisma = new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_CONNECTION_STRING,
-      },
-    },
-  });
-
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    const getMessages = await prisma.message.findMany() as Message[];
+    console.log(process.env.DATABASE_CONNECTION_STRING);
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: getMessages
+        body: process.env.DATABASE_CONNECTION_STRING
     };
 
-    
+
 };
 
 export default httpTrigger;
--- src/App.tsx
+++ src/App.tsx
@@ -33,9 +33,10 @@ function App() {
   const [loading, setLoading] = useState<boolean>(false);
 
   const getMessages = async () => {
-    let result  = await (await fetch(getURL()+"GetAllMessages")).json() as Message[];
-    setMessages(result);
-    setLoading(false);
+    let result  = await (await fetch(getURL()+"GetAllMessages")).text() as string;
+    console.log(result)
+    //setMessages(result);
+    setMessages([{name: "test", message: "sad"}]as Message[]);
   }
 
   const deleteMessage = async (id: number) => {
