5b496c4d28b3644a93e3fca0eeb36ae783ebe710
Aidan Higginbotham <aidan.higginbotham.265714@gmail.com>
2021-05-07 20:48:09 -0500

    feat: FINALLY!

--- api/HttpTrigger/index.ts
+++ api/HttpTrigger/index.ts
@@ -1,20 +1,36 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { PrismaClient } from '@prisma/client';
 
 type Message = {
-    name: string,
-    message: string
-}
+	name: string;
+	message: string;
+};
 
+const prisma = new PrismaClient({
+	datasources: {
+		db: {
+			url: process.env.DATABASE_CONNECTION_STRING,
+		},
+	},
+});
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {   
+const httpTrigger: AzureFunction = async function (
+	context: Context,
+	req: HttpRequest
+): Promise<void> {
+	const createUser = await prisma.message.create({
+		data: {
+			name: req.body.name,
+			message: req.body.message,
+		},
+	});
 
-    const responseMessage = "Success!"
+	const responseMessage = 'Success!';
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-
+	context.res = {
+		// status: 200, /* Defaults to 200 */
+		body: responseMessage,
+	};
 };
 
 export default httpTrigger;
